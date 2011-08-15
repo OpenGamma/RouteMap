d791c1f8d478cb0d7fb01ad139a35d6682be0505
Afshin Darian <afshin@opengamma.com>
2011-08-15 19:36:28 +0100

    small change in node testing script

--- tests/tests-node.js
+++ tests/tests-node.js
@@ -1,4 +1,4 @@
-var qunit = require('./lib/qunit/qunit').QUnit, all_passed;
+var qunit = require('./lib/qunit/qunit').QUnit, all_passed, global_passed;
 qunit.log = function (output) {
     var message = (output.result ? 'PASS: ' : 'FAIL: ') + output.message + '\n';
     if (!output.result) all_passed = false;
@@ -8,11 +8,16 @@ qunit.log = function (output) {
     console[output.result ? 'log' : 'error'](message);
 };
 console.log('Tests for routemap.js');
-all_passed = true;
+global_passed = all_passed = true;
 require('./lib/tests').RouteMapTests(qunit, require('../routemap').RouteMap);
-console[all_passed ? 'log' : 'error'](all_passed ? 'ALL TESTS PASSED' : '\033[31mSOME TESTS FAILED\033[39m');
+console[all_passed ? 'log' : 'error']('Tests for routemap.js => ' + 
+    (all_passed ? 'ALL TESTS PASSED' : '\033[31mSOME TESTS FAILED\033[39m'));
 console.log('Tests for routemap.compressed.js');
+global_passed = all_passed;
 all_passed = true;
 require('./lib/tests').RouteMapTests(qunit, require('../routemap.compressed').RouteMap);
-console[all_passed ? 'log' : 'error'](all_passed ? 'ALL TESTS PASSED' : '\033[31mSOME TESTS FAILED\033[39m');
-process.exit(all_passed ? 0 : 1);
+console[all_passed ? 'log' : 'error']('Tests for routemap.compressed.js => ' + 
+    (all_passed ? 'ALL TESTS PASSED' : '\033[31mSOME TESTS FAILED\033[39m'));
+global_passed = global_passed && all_passed;
+if (!global_passed) console.error('\n\n\n\033[31mONE OR BOTH FILES FAILED\033[39m');
+process.exit(global_passed ? 0 : 1);
